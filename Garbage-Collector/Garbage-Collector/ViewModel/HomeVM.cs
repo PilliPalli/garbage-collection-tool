2f028ed7844b7c269f9da0f58cfb5eb6203dc8d1
Moritz Nicola Kreis <110674069+PilliPalli@users.noreply.github.com>
2024-09-12 17:45:38 +0200

    new close_app button and possibilty to download cleanup stats

--- Garbage-Collector/Garbage-Collector/ViewModel/HomeVM.cs
+++ Garbage-Collector/Garbage-Collector/ViewModel/HomeVM.cs
@@ -1,7 +1,8 @@
 ﻿using Garbage_Collector.Model;
 using Garbage_Collector.Utilities;
+using Microsoft.Win32;
 using System.Collections.ObjectModel;
-using System.Linq;
+using System.IO;
 using System.Windows.Input;
 
 namespace Garbage_Collector.ViewModel
@@ -20,6 +21,20 @@ namespace Garbage_Collector.ViewModel
             {
                 _cleanupLogs = value;
                 OnPropertyChanged();
+            }
+        }
+
+        private string _statusMessage;
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set
+            {
+                if (_statusMessage != value)
+                {
+                    _statusMessage = value;
+                    OnPropertyChanged();
+                }
             }
         }
 
@@ -54,12 +69,14 @@ namespace Garbage_Collector.ViewModel
         }
 
         public ICommand ClearStatisticsCommand { get; }
+        public ICommand ExportLogsCommand { get; }
 
         public HomeVM()
         {
             LoadCleanupLogs();
             WelcomeMessage = $"Willkommen, {LoginVM.CurrentUserName}!";
             ClearStatisticsCommand = new RelayCommand(ClearStatistics);
+            ExportLogsCommand = new RelayCommand(ExportLogs);
             CalculateTotals();
         }
 
@@ -68,7 +85,8 @@ namespace Garbage_Collector.ViewModel
             using (var context = new GarbageCollectorDbContext())
             {
                 var logs = context.CleanupLogs
-                                              .OrderByDescending(log => log.CleanupDate) 
+                                              .Where(log => log.UserId == LoginVM.CurrentUserId)
+                                              .OrderByDescending(log => log.CleanupDate)
                                               .Take(5)
                                               .ToList();
 
@@ -87,14 +105,70 @@ namespace Garbage_Collector.ViewModel
             CalculateTotals();
         }
 
+        private void ExportLogs(object obj)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                FileName = "cleanup_logs",
+                DefaultExt = ".csv",
+                Filter = "CSV-Dateien (.csv)|*.csv"
+            };
+
+            bool? result = saveFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string exportFilePath = saveFileDialog.FileName;
+
+                try
+                {
+                    using (var writer = new StreamWriter(exportFilePath))
+                    {
+                       
+                        writer.WriteLine("Datum,Gelöschte Dateien,Freigegebener Speicher (MB),Bereinigungstyp");
+
+                        using (var context = new GarbageCollectorDbContext())
+                        {
+                           
+                            var userLogs = context.CleanupLogs
+                                                  .Where(log => log.UserId == LoginVM.CurrentUserId)
+                                                  .OrderByDescending(log => log.CleanupDate)
+                                                  .ToList();
+
+                           
+                            foreach (var log in userLogs)
+                            {
+                                writer.WriteLine($"{log.CleanupDate};{log.FilesDeleted};{log.SpaceFreedInMb};{log.CleanupType}");
+                            }
+                        }
+                    }
+
+                    StatusMessage = "Logs wurden erfolgreich exportiert.";
+                }
+                catch (Exception ex)
+                {
+                    StatusMessage = $"Fehler beim Exportieren der Logs: {ex.Message}";
+                }
+
+                OnPropertyChanged(nameof(StatusMessage));
+            }
+        }
+
+
+
         private void CalculateTotals()
         {
             using (var context = new GarbageCollectorDbContext())
             {
-                var allLogs = context.CleanupLogs.ToList();
-                TotalDeletedFiles = allLogs.Sum(log => log.FilesDeleted).ToString();
-                TotalFreedSpace = allLogs.Sum(log => log.SpaceFreedInMb).ToString("F2") + " MB";
+                
+                var userLogs = context.CleanupLogs
+                                      .Where(log => log.UserId == LoginVM.CurrentUserId)
+                                      .ToList();
+
+                TotalDeletedFiles = userLogs.Sum(log => log.FilesDeleted).ToString();
+                TotalFreedSpace = userLogs.Sum(log => log.SpaceFreedInMb).ToString("F2") + " MB";
             }
         }
+
     }
 }
